6d43ebce787bf8a59259d8d643e92fc83440e2b6
Akbar Wijaya <151990608+akbarwjyy@users.noreply.github.com>
2026-01-21 23:25:41 +0700

    format code in hello.go for improved readability

--- hello/hello.go
+++ hello/hello.go
@@ -1,28 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "example.com/greetings"
+	"example.com/greetings"
 )
 
 func main() {
-    // Set properties of the predefined Logger, including
-    // the log entry prefix and a flag to disable printing
-    // the time, source file, and line number.
-    log.SetPrefix("greetings: ")
-    log.SetFlags(0)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
-    // A slice of names.
-    names := []string{"Gladys", "Samantha", "Darrin"}
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
-    // Request greeting messages for the names.
-    messages, err := greetings.Hellos(names)
-    if err != nil {
-        log.Fatal(err)
-    }
-    // If no error was returned, print the returned map of
-    // messages to the console.
-    fmt.Println(messages)
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
+
+	var inputString string
+
+	// Membaca satu baris input dari STDIN
+	fmt.Scanln(&inputString)
+
+	// Mencetak output sesuai ketentuan
+	fmt.Println("Hello, World.")
+	fmt.Println(inputString)
 }
